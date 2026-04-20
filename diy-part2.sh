b1c01dc937d3a9e0b48809522918574a65e3de9c
ssxnqtggshn <ssxnqtggshn@yahoo.com>
2026-04-20 21:13:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,7 +2,7 @@
 # 描述: 更新 feeds 后的自定义脚本
 
 # 示例：修改默认后台 IP 为 192.168.10.1
-#sed -i 's/192.168.6.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
 # 示例：修改主机名
 sed -i 's/ImmortalWrt/GL-MT6000/g' package/base-files/files/bin/config_generate
